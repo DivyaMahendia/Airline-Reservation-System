9fbc151f5a893d75c170aeab517403206552d03e
DivyaMahendia <divyamahendia7346@gmail.com>
2018-11-16 03:59:34 +0530

    Initial

---
+++ AIRLINE-RESERVATION-SYSTEM.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+#include<string>
+#include<windows.h>
+#include<fstream>
+using namespace std;
+
+int main()
+{SetConsoleTitle("AIRLINE RESERVATION");
+ system("color 0f");
+ cout<<"\n\n\n\n\n\n\n\n\n\n\t\t     WELCOME TO AIRLINE RESERVATION SYSTEM";
+ Sleep(1000);
+ system ("cls");
+ cout<<"\n\n\n\n\n\n\n\t\t          Developed by :";
+ Sleep(500);
+  cout<<" Divya Mahendia";
+
+ Sleep(3000);
+ system("cls");
+
+}
